479959c358563c76f93e03291c5b8a7a3a873226
parkhometsN <parkhometsnikita@gmail.com>
2026-04-17 22:52:58 +0300

    up0.6

--- frontend/src/pages/app/Dashboard.jsx
+++ frontend/src/pages/app/Dashboard.jsx
@@ -1,7 +1,6 @@
 export default function Dashboard() {
   return (
     <div className="app_pages">
-      
       <div className="app_content">
         <div className="app_items">
           <h1>Портфель</h1>
